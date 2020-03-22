eb2dd09aeff40ed3f21151937fc6dfc80e3b4ab7
olabukoye Oluwafemi <fmgold2015@gmail.com>
2020-03-22 08:56:57 -1100

    Updates

---
+++ precache-manifest.9c1eef530017206abe4897ac4bb182d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0bc1f71c692e498d78b67cbce06c0a",
+    "url": "/Venue/index.html"
+  },
+  {
+    "revision": "1a859c9c29a5fef484a8",
+    "url": "/Venue/static/css/main.0d1eaf94.chunk.css"
+  },
+  {
+    "revision": "6f1bec0e70b19cd08d55",
+    "url": "/Venue/static/js/2.03b322c9.chunk.js"
+  },
+  {
+    "revision": "1a859c9c29a5fef484a8",
+    "url": "/Venue/static/js/main.94a2cc04.chunk.js"
+  },
+  {
+    "revision": "87398a12f3a4e0b1e1f6",
+    "url": "/Venue/static/js/runtime-main.69854329.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/Venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/Venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/Venue/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
